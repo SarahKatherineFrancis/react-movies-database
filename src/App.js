6e8851813be161dd76d890c674344f380ac77691
Sarah Francis <sarah.katherinefrancis@gmail.com>
2023-09-11 16:39:05 -0400

    Handle loading and errors during data fetching

--- src/App.js
+++ src/App.js
@@ -5,32 +5,57 @@ import "./App.css";
 
 // Define the main App component
 function App() {
-  // Define state variables 'movies' and 'isLoading' and functions to manage them
+  // Define state variables 'movies', 'isLoading', 'error', and functions to manage them
   const [movies, setMovies] = useState([]); // State for storing movie data
   const [isLoading, setIsLoading] = useState(false); // State for managing loading state
+  const [error, setError] = useState(null); // State for handling errors during data fetching
 
   // Define an asynchronous function to fetch movies data from an external API
   async function fetchMoviesHandler() {
     setIsLoading(true); // Set loading state to true while fetching data
-    // Send an HTTP GET request to the SWAPI (Star Wars API) to retrieve movie data
-    const response = await fetch("https://swapi.dev/api/films");
+    setError(null); // Clear any previous error messages
+    try {
+      // Send an HTTP GET request to the SWAPI (Star Wars API) to retrieve movie data
+      const response = await fetch("https://swapi.dev/api/films");
 
-    // Parse the response data as JSON (assuming the API returns JSON data)
-    const data = await response.json();
+      if (!response.ok) {
+        throw new Error("Something went wrong!"); // Throw an error if the response is not OK
+      }
 
-    // Transform the retrieved movie data into a more usable format
-    const transformedMovies = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date,
-      };
-    });
+      // Parse the response data as JSON (assuming the API returns JSON data)
+      const data = await response.json();
 
-    // Update the 'movies' state with the transformed movie data
-    setMovies(transformedMovies);
+      // Transform the retrieved movie data into a more usable format
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+
+      // Update the 'movies' state with the transformed movie data
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message); // Handle and store any errors that occur during data fetching
+    }
     setIsLoading(false); // Set loading state back to false after data is fetched
+  }
+
+  let content = <p>Found no movies.</p>;
+
+  // Determine the content to render based on the current state
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
   }
 
   // Render the App component
@@ -40,12 +65,7 @@ function App() {
         {/* Render a button that, when clicked, triggers the 'fetchMoviesHandler' function */}
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section>
-        {/* Conditional rendering based on loading and movie data availability */}
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
-        {isLoading && <p>Loading...</p>}
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
